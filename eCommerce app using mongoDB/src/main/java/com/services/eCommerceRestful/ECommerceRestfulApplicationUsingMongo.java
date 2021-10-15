fb514883bc9dd5bae5989f1cb05df88cccb874fd
Jay <50858217+jaysampath@users.noreply.github.com>
2021-10-15 15:47:04 +0530

    Add files via upload

---
+++ eCommerce app using mongoDB/src/main/java/com/services/eCommerceRestful/ECommerceRestfulApplicationUsingMongo.java
@@ -0,0 +1,13 @@
+package com.services.eCommerceRestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceRestfulApplicationUsingMongo {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceRestfulApplicationUsingMongo.class, args);
+	}
+
+}
